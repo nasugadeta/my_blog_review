7c59f2aab382edbd164a38fe9a28736196d188ab
松永寛史 <hiromatsu0707@gmail.com>
2015-06-18 22:03:06 +0900

    新規記事投稿機能を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,21 @@
 class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to "/posts/#{@post.id}"
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(
+      :title,
+      :body,
+      :category
+      )
+  end
 end
