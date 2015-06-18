7c63de91ff4dbba938e4665cc95d14fdaf558cbd
松永寛史 <hiromatsu0707@gmail.com>
2015-06-18 23:39:20 +0900

    記事削除リンクを生成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,6 +27,12 @@ class PostsController < ApplicationController
     redirect_to "/posts/#{@post.id}"
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to "/"
+  end
+
   private
 
   def post_params
